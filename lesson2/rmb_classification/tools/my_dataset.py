26fa32b45074e6b787329ab68aa9791bb93b271f
zhang xian <1756569043@qq.com>
2020-05-21 23:21:57 +0800

    add custom dataset

---
+++ lesson2/rmb_classification/tools/my_dataset.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+import os
+import random
+from PIL import Image
+from torch.utils.data import Dataset
+
+random.seed(1)
+rmb_label = {"1": 0, "100": 1}
+
+class RMBDataset(Dataset):
+    def __init__(self, data_dir, transform=None):
+        """
+        rmb面额分类任务的Dataset
+        :param data_dir: str, 数据集所在路径
+        :param transform: torch.transform，数据预处理
+        """
+        # data_info存储所有图片路径和标签，在DataLoader中通过index读取样本
+        self.data_info = self.get_img_info(data_dir)
+        self.transform = transform
+
+    def __getitem__(self, index):
+        # 通过 index 读取样本
+        path_img, label = self.data_info[index]
+        # 注意这里需要 convert('RGB')
+        img = Image.open(path_img).convert('RGB')     # 0~255
+        if self.transform is not None:
+            img = self.transform(img)   # 在这里做transform，转为tensor等等
+        # 返回是样本和标签
+        return img, label
+
+    # 返回所有样本的数量
+    def __len__(self):
+        return len(self.data_info)
+
+    @staticmethod
+    def get_img_info(data_dir):
+        data_info = list()
+        # data_dir 是训练集、验证集或者测试集的路径
+        for root, dirs, _ in os.walk(data_dir):
+            # 遍历类别
+            # dirs ['1', '100']
+            for sub_dir in dirs:
+                # 文件列表
+                img_names = os.listdir(os.path.join(root, sub_dir))
+                # 取出 jpg 结尾的文件
+                img_names = list(filter(lambda x: x.endswith('.jpg'), img_names))
+                # 遍历图片
+                for i in range(len(img_names)):
+                    img_name = img_names[i]
+                    # 图片的绝对路径
+                    path_img = os.path.join(root, sub_dir, img_name)
+                    # 标签，这里需要映射为 0、1 两个类别
+                    label = rmb_label[sub_dir]
+                    # 保存在 data_info 变量中
+                    data_info.append((path_img, int(label)))
+        return data_info
