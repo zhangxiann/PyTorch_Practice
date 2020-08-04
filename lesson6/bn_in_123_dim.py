7e01304d080cbf0cea8b94f25500d870b04b19a2
zhang xian <1756569043@qq.com>
2020-08-04 12:03:02 +0800

    讲解 Layer Normalization、Group Normalization等代码

---
+++ lesson6/bn_in_123_dim.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+# @file name  : bn_and_initialize.py
+# @date       : 2019-11-01
+# @brief      : bn于权值初始化
+"""
+import torch
+import numpy as np
+import torch.nn as nn
+from common_tools import set_seed
+
+
+
+set_seed(1)  # 设置随机种子
+
+# ======================================== nn.BatchNorm1d
+# flag = 1
+flag = 0
+if flag:
+
+    batch_size = 3
+    num_features = 5
+    momentum = 0.3
+
+    features_shape = (1)
+
+    feature_map = torch.ones(features_shape)                                                    # 1D
+    feature_maps = torch.stack([feature_map*(i+1) for i in range(num_features)], dim=0)         # 2D
+    feature_maps_bs = torch.stack([feature_maps for i in range(batch_size)], dim=0)             # 3D
+    print("input data:\n{} shape is {}".format(feature_maps_bs, feature_maps_bs.shape))
+
+    bn = nn.BatchNorm1d(num_features=num_features, momentum=momentum)
+
+    running_mean, running_var = 0, 1
+    mean_t, var_t = 2, 0 # 表示第二个特征维度的均值和方差。
+    for i in range(2):
+        outputs = bn(feature_maps_bs)
+
+        print("\niteration:{}, running mean: {} ".format(i, bn.running_mean))
+        print("iteration:{}, running var:{} ".format(i, bn.running_var))
+
+        running_mean = (1 - momentum) * running_mean + momentum * mean_t
+        running_var = (1 - momentum) * running_var + momentum * var_t
+
+        print("iteration:{}, 第二个特征的running mean: {} ".format(i, running_mean))
+        print("iteration:{}, 第二个特征的running var:{}".format(i, running_var))
+
+# ======================================== nn.BatchNorm2d
+# flag = 1
+flag = 0
+if flag:
+
+    batch_size = 3
+    num_features = 3
+    momentum = 0.3
+    
+    features_shape = (2, 2)
+
+    feature_map = torch.ones(features_shape)                                                    # 2D
+    feature_maps = torch.stack([feature_map*(i+1) for i in range(num_features)], dim=0)         # 3D
+    feature_maps_bs = torch.stack([feature_maps for i in range(batch_size)], dim=0)             # 4D
+
+    # print("input data:\n{} shape is {}".format(feature_maps_bs, feature_maps_bs.shape))
+
+    bn = nn.BatchNorm2d(num_features=num_features, momentum=momentum)
+
+    running_mean, running_var = 0, 1
+
+    for i in range(2):
+        outputs = bn(feature_maps_bs)
+
+        print("\niter:{}, running_mean: {}".format(i, bn.running_mean))
+        print("iter:{}, running_var: {}".format(i, bn.running_var))
+
+        print("iter:{}, weight: {}".format(i, bn.weight.data.numpy()))
+        print("iter:{}, bias: {}".format(i, bn.bias.data.numpy()))
+
+# ======================================== nn.BatchNorm3d
+flag = 1
+# flag = 0
+if flag:
+
+    batch_size = 3
+    num_features = 3
+    momentum = 0.3
+
+    features_shape = (2, 2, 3)
+
+    feature = torch.ones(features_shape)                                                # 3D
+    feature_map = torch.stack([feature * (i + 1) for i in range(num_features)], dim=0)  # 4D
+    feature_maps = torch.stack([feature_map for i in range(batch_size)], dim=0)         # 5D
+
+    # print("input data:\n{} shape is {}".format(feature_maps, feature_maps.shape))
+
+    bn = nn.BatchNorm3d(num_features=num_features, momentum=momentum)
+
+    running_mean, running_var = 0, 1
+
+    for i in range(2):
+        outputs = bn(feature_maps)
+
+        print("\niter:{}, running_mean.shape: {}".format(i, bn.running_mean.shape))
+        print("iter:{}, running_var.shape: {}".format(i, bn.running_var.shape))
+
+        print("iter:{}, weight.shape: {}".format(i, bn.weight.shape))
+        print("iter:{}, bias.shape: {}".format(i, bn.bias.shape))
+
+
+
+
+
+
---
+++ lesson6/normallization_layers.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+# @file name  : bn_and_initialize.py
+# @date       : 2019-11-03
+# @brief      : pytorch中常见的 normalization layers
+"""
+import torch
+import torch.nn as nn
+from common_tools import set_seed
+
+
+set_seed(1)  # 设置随机种子
+
+# ======================================== nn.layer norm
+# flag = 1
+flag = 0
+if flag:
+    batch_size = 8
+    num_features = 2
+
+    features_shape = (3, 4)
+
+    feature_map = torch.ones(features_shape)  # 2D
+    feature_maps = torch.stack([feature_map * (i + 1) for i in range(num_features)], dim=0)  # 3D
+    feature_maps_bs = torch.stack([feature_maps for i in range(batch_size)], dim=0)  # 4D
+
+    # feature_maps_bs shape is [8, 6, 3, 4],  B * C * H * W
+    # ln = nn.LayerNorm(feature_maps_bs.size()[1:], elementwise_affine=True)
+    # ln = nn.LayerNorm(feature_maps_bs.size()[1:], elementwise_affine=False)
+    # ln = nn.LayerNorm([6, 3, 4])
+    ln = nn.LayerNorm([4])
+
+    output = ln(feature_maps_bs)
+
+    print("Layer Normalization")
+    print(ln.weight.shape)
+    print(feature_maps_bs[0, ...])
+    print(output[0, ...])
+
+# ======================================== nn.instance norm 2d
+# flag = 1
+flag = 0
+if flag:
+
+    batch_size = 3
+    num_features = 3
+    momentum = 0.3
+
+    features_shape = (2, 2)
+
+    feature_map = torch.ones(features_shape)    # 2D
+    feature_maps = torch.stack([feature_map * (i + 1) for i in range(num_features)], dim=0)  # 3D
+    feature_maps_bs = torch.stack([feature_maps for i in range(batch_size)], dim=0)  # 4D
+
+    print("Instance Normalization")
+    print("input data:\n{} shape is {}".format(feature_maps_bs, feature_maps_bs.shape))
+
+    instance_n = nn.InstanceNorm2d(num_features=num_features, momentum=momentum)
+
+    for i in range(1):
+        outputs = instance_n(feature_maps_bs)
+
+        print(outputs)
+
+
+
+# ======================================== nn.grop norm
+flag = 1
+# flag = 0
+if flag:
+
+    batch_size = 2
+    num_features = 4
+    num_groups = 2
+    features_shape = (2, 2)
+
+    feature_map = torch.ones(features_shape)    # 2D
+    feature_maps = torch.stack([feature_map * (i + 1) for i in range(num_features)], dim=0)  # 3D
+    feature_maps_bs = torch.stack([feature_maps * (i + 1) for i in range(batch_size)], dim=0)  # 4D
+
+    gn = nn.GroupNorm(num_groups, num_features)
+    outputs = gn(feature_maps_bs)
+
+    print("Group Normalization")
+    print(gn.weight.shape)
+    print(outputs[0])
+
+
+
+
+
+
