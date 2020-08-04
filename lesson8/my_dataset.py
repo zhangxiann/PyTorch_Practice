ceaefc368766b450b229397f3589be2e58c3570b
zhang xian <1756569043@qq.com>
2020-08-04 12:05:35 +0800

    讲解图像分类

---
+++ lesson8/my_dataset.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+# @file name  : dataset.py
+# @date       : 2019-08-21 10:08:00
+# @brief      : 各数据集的Dataset定义
+"""
+import numpy as np
+import torch
+import os
+import random
+from PIL import Image
+from torch.utils.data import Dataset
+
+random.seed(1)
+rmb_label = {"1": 0, "100": 1}
+
+
+class PennFudanDataset(object):
+    def __init__(self, data_dir, transforms):
+
+        self.data_dir = data_dir
+        self.transforms = transforms
+        self.img_dir = os.path.join(data_dir, "PNGImages")
+        self.txt_dir = os.path.join(data_dir, "Annotation")
+        # 保存所有图片的文件名，后面用于查找对应的 txt 标签文件
+        self.names = [name[:-4] for name in list(filter(lambda x: x.endswith(".png"), os.listdir(self.img_dir)))]
+
+    def __getitem__(self, index):
+        """
+        返回img和target
+        :param idx:
+        :return:
+        """
+
+        name = self.names[index]
+        path_img = os.path.join(self.img_dir, name + ".png")
+        path_txt = os.path.join(self.txt_dir, name + ".txt")
+
+        # load img
+        img = Image.open(path_img).convert("RGB")
+
+        # load boxes and label
+        f = open(path_txt, "r")
+        import re
+        # 查找每一行是否有数字，有数字的则是带有标签的行
+        points = [re.findall(r"\d+", line) for line in f.readlines() if "Xmin" in line]
+        boxes_list = list()
+        for point in points:
+            box = [int(p) for p in point]
+            boxes_list.append(box[-4:])
+        boxes = torch.tensor(boxes_list, dtype=torch.float)
+        labels = torch.ones((boxes.shape[0],), dtype=torch.long)
+
+        # iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+        target = {}
+        # 组成 label，是一个 dict，包括 boxes 和 labels
+        target["boxes"] = boxes
+        target["labels"] = labels
+        # target["iscrowd"] = iscrowd
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+    def __len__(self):
+        if len(self.names) == 0:
+            raise Exception("\ndata_dir:{} is a empty dir! Please checkout your path to images!".format(data_dir))
+        return len(self.names)
+
+class CelebADataset(object):
+
+    def __init__(self, data_dir, transforms):
+
+        self.data_dir = data_dir
+        self.transform = transforms
+        self.img_names = [name for name in list(filter(lambda x: x.endswith(".jpg"), os.listdir(self.data_dir)))]
+
+    def __getitem__(self, index):
+        path_img = os.path.join(self.data_dir, self.img_names[index])
+        img = Image.open(path_img).convert('RGB')
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img
+
+    def __len__(self):
+        if len(self.img_names) == 0:
+            raise Exception("\ndata_dir:{} is a empty dir! Please checkout your path to images!".format(self.data_dir))
+        return len(self.img_names)
---
+++ lesson8/resnet_inference.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+"""
+# @file name  : resnet_inference.py
+# @date       : 2019-11-16
+# @brief      : inference demo
+"""
+
+import os
+import time
+import torch.nn as nn
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+from matplotlib import pyplot as plt
+import torchvision.models as models
+import enviroments
+import torchsummary
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
+
+# config
+vis = True
+# vis = False
+vis_row = 4
+
+norm_mean = [0.485, 0.456, 0.406]
+norm_std = [0.229, 0.224, 0.225]
+
+inference_transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(norm_mean, norm_std),
+])
+
+classes = ["ants", "bees"]
+
+
+def img_transform(img_rgb, transform=None):
+    """
+    将数据转换为模型读取的形式
+    :param img_rgb: PIL Image
+    :param transform: torchvision.transform
+    :return: tensor
+    """
+
+    if transform is None:
+        raise ValueError("找不到transform！必须有transform对img进行处理")
+
+    img_t = transform(img_rgb)
+    return img_t
+
+
+def get_img_name(img_dir, format="jpg"):
+    """
+    获取文件夹下format格式的文件名
+    :param img_dir: str
+    :param format: str
+    :return: list
+    """
+    file_names = os.listdir(img_dir)
+    # 使用 list(filter(lambda())) 筛选出 jpg 后缀的文件
+    img_names = list(filter(lambda x: x.endswith(format), file_names))
+
+    if len(img_names) < 1:
+        raise ValueError("{}下找不到{}格式数据".format(img_dir, format))
+    return img_names
+
+
+def get_model(m_path, vis_model=False):
+
+    resnet18 = models.resnet18()
+    # torchsummary.summary(resnet18, (3,224,224))
+    # 修改全连接层的输出
+    num_ftrs = resnet18.fc.in_features
+    resnet18.fc = nn.Linear(num_ftrs, 2)
+
+    # 加载模型参数
+    checkpoint = torch.load(m_path)
+    resnet18.load_state_dict(checkpoint['model_state_dict'])
+
+
+    if vis_model:
+        from torchsummary import summary
+        summary(resnet18, input_size=(3, 224, 224), device="cpu")
+
+    return resnet18
+
+
+if __name__ == "__main__":
+
+    img_dir = os.path.join(enviroments.hymenoptera_data_dir,"val/bees")
+    model_path = "./checkpoint_14_epoch.pkl"
+    time_total = 0
+    img_list, img_pred = list(), list()
+
+    # 1. data
+    img_names = get_img_name(img_dir)
+    num_img = len(img_names)
+
+    # 2. model
+    resnet18 = get_model(model_path, True)
+    resnet18.to(device)
+    resnet18.eval()
+
+    with torch.no_grad():
+        for idx, img_name in enumerate(img_names):
+
+            path_img = os.path.join(img_dir, img_name)
+
+            # step 1/4 : path --> img
+            img_rgb = Image.open(path_img).convert('RGB')
+
+            # step 2/4 : img --> tensor
+            img_tensor = img_transform(img_rgb, inference_transform)
+            img_tensor.unsqueeze_(0)
+            img_tensor = img_tensor.to(device)
+
+            # step 3/4 : tensor --> vector
+            time_tic = time.time()
+            outputs = resnet18(img_tensor)
+            time_toc = time.time()
+
+            # step 4/4 : visualization
+            _, pred_int = torch.max(outputs.data, 1)
+            pred_str = classes[int(pred_int)]
+
+            if vis:
+                img_list.append(img_rgb)
+                img_pred.append(pred_str)
+
+                if (idx+1) % (vis_row*vis_row) == 0 or num_img == idx+1:
+                    for i in range(len(img_list)):
+                        plt.subplot(vis_row, vis_row, i+1).imshow(img_list[i])
+                        plt.title("predict:{}".format(img_pred[i]))
+                    plt.show()
+                    plt.close()
+                    img_list, img_pred = list(), list()
+
+            time_s = time_toc-time_tic
+            time_total += time_s
+
+            print('{:d}/{:d}: {} {:.3f}s '.format(idx + 1, num_img, img_name, time_s))
+
+    print("\ndevice:{} total time:{:.1f}s mean:{:.3f}s".
+          format(device, time_total, time_total/num_img))
+    if torch.cuda.is_available():
+        print("GPU name:{}".format(torch.cuda.get_device_name()))
+
