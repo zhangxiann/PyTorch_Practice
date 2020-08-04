ba00856071ef2759c689af9836b66c9efc7c8416
zhang xian <1756569043@qq.com>
2020-08-04 12:04:01 +0800

    讲解模型保存与加载

---
+++ lesson7/model_load.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+# @file name  : model_load.py
+# @date       : 2019-11-04
+# @brief      : 模型的加载
+"""
+import torch
+import numpy as np
+import torch.nn as nn
+from common_tools import set_seed
+
+
+class LeNet2(nn.Module):
+    def __init__(self, classes):
+        super(LeNet2, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 6, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(6, 16, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(16*5*5, 120),
+            nn.ReLU(),
+            nn.Linear(120, 84),
+            nn.ReLU(),
+            nn.Linear(84, classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size()[0], -1)
+        x = self.classifier(x)
+        return x
+
+    def initialize(self):
+        for p in self.parameters():
+            p.data.fill_(20191104)
+
+
+# ================================== load net ===========================
+# flag = 1
+flag = 0
+if flag:
+
+    path_model = "./model.pkl"
+    net_load = torch.load(path_model)
+
+    print(net_load)
+
+# ================================== load state_dict ===========================
+
+flag = 1
+# flag = 0
+if flag:
+
+    path_state_dict = "./model_state_dict.pkl"
+    state_dict_load = torch.load(path_state_dict)
+
+    print(state_dict_load.keys())
+
+# ================================== update state_dict ===========================
+flag = 1
+# flag = 0
+if flag:
+
+    net_new = LeNet2(classes=2019)
+
+    print("加载前: ", net_new.features[0].weight[0, ...])
+    net_new.load_state_dict(state_dict_load)
+    print("加载后: ", net_new.features[0].weight[0, ...])
+
+
+
+
+
---
+++ lesson7/model_save.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+# @file name  : model_save.py
+# @date       : 2019-11-04
+# @brief      : 模型的保存
+"""
+import torch
+import numpy as np
+import torch.nn as nn
+from common_tools import set_seed
+
+
+class LeNet2(nn.Module):
+    def __init__(self, classes):
+        super(LeNet2, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 6, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(6, 16, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(16*5*5, 120),
+            nn.ReLU(),
+            nn.Linear(120, 84),
+            nn.ReLU(),
+            nn.Linear(84, classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size()[0], -1)
+        x = self.classifier(x)
+        return x
+
+    def initialize(self):
+        for p in self.parameters():
+            p.data.fill_(2020)
+
+
+net = LeNet2(classes=2019)
+
+# "训练"
+print("训练前: ", net.features[0].weight[0, ...])
+net.initialize()
+print("训练后: ", net.features[0].weight[0, ...])
+
+path_model = "./model.pkl"
+path_state_dict = "./model_state_dict.pkl"
+
+# 保存整个模型
+torch.save(net, path_model)
+
+# 保存模型参数
+net_state_dict = net.state_dict()
+torch.save(net_state_dict, path_state_dict)
+
+
+
+
+
+
+
